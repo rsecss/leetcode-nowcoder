28c3df09290e44c2755491c06759a0d3477478f3
zhanghaochuan <132198239+rsecss@users.noreply.github.com>
2024-07-15 23:39:29 +0800

    finish 111

--- nowcoder/C/induction.c
+++ nowcoder/C/induction.c
@@ -3173,4 +3173,29 @@ int main()
     return 0;
 }
 
+//BC111 小乐乐与进制转换
+#include <stdio.h>
+
+//转换方法
+void print(int x)
+{
+    if(x > 5)
+    {
+        print(x/6);
+    }
+    printf("%d",x%6);
+}
+
+int main()
+{
+    int n  = 0;
+
+    while(scanf("%d",&n) != EOF)
+    {
+        print(n);
+    }
+
+    return 0;
+}
+
 //
