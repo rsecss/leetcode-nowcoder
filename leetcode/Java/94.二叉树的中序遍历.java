e86ebd20d549b6b9890f70e23c08ea0e5234d616
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-08-29 19:01:13 +0800

    finish leetcode "94. 二叉树的中序遍历"

---
+++ leetcode/Java/94.二叉树的中序遍历.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// 中序遍历（左根右）
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        inorder(root,result);
+        return result;
+    }
+
+    public void inorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, result);
+        result.add(root.val); // 注意这里跟前序遍历不一样
+        inorder(root.right, result);
+    }
+}
+// @lc code=end
+
