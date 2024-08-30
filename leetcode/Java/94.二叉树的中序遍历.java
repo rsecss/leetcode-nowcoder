9448edf5fe90ef9ee112c78c7a8d342b8c25a4c7
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-08-30 18:29:05 +0800

    update leetcode "94. 二叉树的中序遍历"

--- leetcode/Java/94.二叉树的中序遍历.java
+++ leetcode/Java/94.二叉树的中序遍历.java
@@ -20,7 +20,7 @@
  *     }
  * }
  */
-// 中序遍历（左根右）
+// 中序遍历（左根右）——递归
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
@@ -35,6 +35,30 @@ class Solution {
         inorder(root.left, result);
         result.add(root.val); // 注意这里跟前序遍历不一样
         inorder(root.right, result);
+    }
+}
+
+// 中序遍历（左根右），入栈顺序：左右——迭代
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return result;
     }
 }
 // @lc code=end
