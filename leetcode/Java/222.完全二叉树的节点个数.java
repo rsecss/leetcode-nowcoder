d2cc594c0ab74e9e720e01644defefd59507ac52
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-09-08 19:18:55 +0800

    finish leetcode "222. 完全二叉树的节点个数"

---
+++ leetcode/Java/222.完全二叉树的节点个数.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// 递归法
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
+
+// 迭代法
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int result = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                result++;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
