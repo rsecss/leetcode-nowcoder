032ce015dc8c3841350d095152a5b23a1cc32277
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-10-09 22:55:10 +0800

    finish leetcode "501. 二叉搜索树中的众数"

---
+++ leetcode/Java/501.二叉搜索树中的众数.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=501 lang=java
+ *
+ * [501] 二叉搜索树中的众数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// 中序遍历
+class Solution {
+    ArrayList<Integer> resList;
+    int maxCount;
+    int count;
+    TreeNode pre;
+
+    public int[] findMode(TreeNode root) {
+        resList = new ArrayList<>();
+        maxCount = 0;
+        count = 0;
+        pre = null;
+        getFindMode(root);
+        int[] res = new int[resList.size()];
+        for (int i = 0; i < resList.size(); i++) {
+            res[i] = resList.get(i);
+        }
+
+        return res;
+    }
+
+    public void getFindMode(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        getFindMode(root.left);
+
+        int rootValue = root.val;
+        // 计数
+        if (pre == null || rootValue != pre.val) {
+            count = 1;
+        } else {
+            count++;
+        }
+
+        // 更新结果以及 maxCount
+        if (count > maxCount) {
+            resList.clear();
+            resList.add(rootValue);
+            maxCount = count;
+        } else if (count == maxCount) {
+            resList.add(rootValue);
+        }
+        pre = root;
+
+        getFindMode(root.right);
+    }
+}
+// @lc code=end
