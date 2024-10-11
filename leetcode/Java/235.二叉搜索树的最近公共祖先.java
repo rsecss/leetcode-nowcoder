9362b4b09f39e31315289ff596ad1d9ca859f30d
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-10-11 10:26:11 +0800

    finish leetcode "235. 二叉搜索树的最近公共祖先"

---
+++ leetcode/Java/235.二叉搜索树的最近公共祖先.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=java
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// 递归法
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+}
+
+// 迭代法
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root =root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
+
