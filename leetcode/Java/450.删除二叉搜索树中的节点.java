a16241e8b826d41ad78537ea9bfa7af8018c46c2
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-10-13 21:17:55 +0800

    update leetcode "450. 删除二叉搜索树中的节点"

---
+++ leetcode/Java/450.删除二叉搜索树中的节点.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=java
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left; // 保持左子树始终小于右子树
+                }
+                cur.left = root.left;
+                root = root.right;
+                return root;
+            }
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
